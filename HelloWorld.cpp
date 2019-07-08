ea7d51667368204bfb1a0ec7c1abfb99e37e0838
Mudit Srivastav <mudit.srivastav@intimetec.com>
2019-07-08 15:31:48 +0530

    testing_revert

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -9,5 +9,8 @@ int main() {
 	cin >> number1 >> number2;
 	number3 = number1 + number 2;
 	cout << "\n" << number1 << " + " << number2 << " = " << number3 << endl;
+
+	cout << "this line should be gone";
+
 	return 0;
 }
