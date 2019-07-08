ed1a340801825c570e9a48e8f071ac9aa4eb2570
Mudit Srivastav <mudit.srivastav@intimetec.com>
2019-07-08 15:15:28 +0530

    Updated addition of two numbers

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-	cout << " Hello World " ;
+	cout << " Hello World ";
+	int number1, number 2, number3;
+	cout << " Enter two numbers:-\n" ;
+	cin >> number1 >> number2;
+	number3 = number1 + number 2;
+	cout << "\n" << number1 << " + " << number2 << " = " << number3 << endl;
 	return 0;
 }
