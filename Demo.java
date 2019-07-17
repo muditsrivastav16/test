0738bb7ebdf9a8bf817b1786bdf45436b1b752fc
muditsrivastav16 <32268684+muditsrivastav16@users.noreply.github.com>
2019-07-17 14:01:41 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 class Demo {
 	public static void main(String[] args) {
-		System.out.println("Java file executed successfully"+" " + args[1]);		
+		System.out.println("Java file executed successfully"+" " + args[0]);		
 	}
 }
